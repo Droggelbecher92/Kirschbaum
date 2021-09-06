4491a0f33f26f1dd0ddfdba8c56addfc7dcd4e77
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-06 17:01:35 +0200

    Add Theme

---
+++ frontend/src/Components/Styles.js
@@ -0,0 +1,9 @@
+import {createTheme} from "@material-ui/core";
+import {blue, cyan} from "@material-ui/core/colors";
+
+export const theme = createTheme({
+    palette: {
+        primary: blue,
+        secondary: cyan
+    }
+})
