856f9e566272ab7eefed9c6a76c167f07384a10b
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-06 17:25:01 +0200

    Build router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import { ThemeProvider, Typography } from '@material-ui/core'
-import { theme } from './Components/Styles'
+import { createTheme, ThemeProvider, Typography } from '@material-ui/core'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { blue, cyan } from '@material-ui/core/colors'
+
+const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: cyan,
+  },
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
           <Route path="/login">
             <Typography align="center" color="secondary">
               Moin
             </Typography>
           </Route>
-        </ThemeProvider>
-      </Switch>
-    </Router>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   )
 }
 
