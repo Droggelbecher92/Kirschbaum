47118ee768126e649934a021a13ad5c35d9c8f6a
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-06 17:25:19 +0200

    Removed Styles

--- frontend/src/Components/Styles.js
+++
@@ -1,9 +0,0 @@
-import {createTheme} from "@material-ui/core";
-import {blue, cyan} from "@material-ui/core/colors";
-
-export const theme = createTheme({
-    palette: {
-        primary: blue,
-        secondary: cyan
-    }
-})
