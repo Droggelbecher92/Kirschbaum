5b01fa82c6f4e87830691e61cd3d88a8aac2862b
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-07 07:48:37 +0200

    Removed all Material-UI stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,13 @@
-import { createTheme, ThemeProvider, Typography } from '@material-ui/core'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { blue, cyan } from '@material-ui/core/colors'
-
-const theme = createTheme({
-  palette: {
-    primary: blue,
-    secondary: cyan,
-  },
-})
+import Login from './Pages/Login'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Typography align="center" color="secondary">
-              Moin
-            </Typography>
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   )
 }
 
