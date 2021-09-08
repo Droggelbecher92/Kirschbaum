7fb2b171314a3722e740767260feea9dfcf5d37a
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-08 09:21:05 +0200

    Add Test Config

---
+++ backend/src/test/java/de/lowani/backend/SpringTestContextConfiguration.java
@@ -0,0 +1,30 @@
+package de.lowani.backend;
+
+
+import de.lowani.backend.config.JpaConfig;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import static org.mockito.Mockito.mock;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"de.lowani.backend"})
+@Import({JpaConfig.class})
+@TestConfiguration
+public class SpringTestContextConfiguration {
+
+    public static final String MOCKED_SERVICES_PROFILE = "mockedUserService";
+
+    @Primary
+    @Bean(name = "dataSource", destroyMethod = "shutdown")
+    public EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+}
