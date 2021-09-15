75e7f40f2d1e47b3d9d4ab251ca8ee97219a30c3
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-15 15:11:51 +0200

    Topics and Categories shown in Frontend

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -3,37 +3,48 @@ import { useAuth } from '../Auth/AuthProvider'
 import MainPage from '../Components/MainPage'
 import ChooseField from '../Components/ChooseField'
 import BottomNav from '../Components/BottomNav'
+import Loading from '../Components/Loading'
+import { useEffect, useState } from 'react'
+import { getCategories, getTopics } from '../Services/api-service'
 
 export default function Home() {
-  const { user } = useAuth()
+  const { user, token } = useAuth()
+  const [topics, setTopics] = useState()
+  const [categories, setCategories] = useState()
+
+  useEffect(() => {
+    setupTopics(token).catch(error => console.log(error.message))
+    setupCategories(token).catch(error => console.log(error.message))
+  }, [token])
+
+  const setupTopics = token =>
+    getTopics(token)
+      .then(response => response.data)
+      .then(setTopics)
+  const setupCategories = token =>
+    getCategories(token)
+      .then(response => response.data)
+      .then(setCategories)
+
   while (!user) {
     return <Redirect to="/login" />
+  }
+  while (!categories || !topics) {
+    return (
+      <MainPage>
+        <Loading />
+      </MainPage>
+    )
   }
   return (
     <MainPage>
       <ChooseField>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
-        <button>hi</button>
+        {categories.map(category => (
+          <button>{category.category}</button>
+        ))}
+        {topics.map(topic => (
+          <button>{topic.topic}</button>
+        ))}
       </ChooseField>
       <BottomNav />
     </MainPage>
--- frontend/src/Services/api-service.js
+++ frontend/src/Services/api-service.js
@@ -17,3 +17,9 @@ export const updatePassword = (token, password) =>
 
 export const updateName = (token, username) =>
   axios.put('api/kirschbaum/user/username', { username }, headers(token))
+
+export const getTopics = token =>
+  axios.get('api/kirschbaum/topic', headers(token))
+
+export const getCategories = token =>
+  axios.get('api/kirschbaum/category', headers(token))
