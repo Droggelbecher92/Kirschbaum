27e0839322277c03ca119634c56b187e17cce422
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-22 13:23:07 +0200

    Add Admin Navbar

--- frontend/src/Pages/AdminPage.js
+++ frontend/src/Pages/AdminPage.js
@@ -1,11 +1,12 @@
 import Main from '../Components/Main'
 import Page from '../Components/Page'
 import BottomNavAdmin from '../Components/BottomNavAdmin'
+import { Typography } from '@material-ui/core'
 
 export default function AdminPage() {
   return (
     <Page>
-      <div>Hallo Admin!</div>
+      <Typography variant="h3">Hallo Admin!</Typography>
       <Main></Main>
       <BottomNavAdmin />
     </Page>
