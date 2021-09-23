a3ad4cae84f9bfe0ec899e5cc07d4e7863f3554f
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-09-23 10:54:49 +0200

    Templates for add page

--- frontend/src/Pages/RegisterPage.js
+++ frontend/src/Pages/RegisterPage.js
@@ -1,10 +1,20 @@
 import Page from '../Components/Page'
 import BottomNavAdmin from '../Components/BottomNavAdmin'
+import MainAdmin from '../Components/MainAdmin'
+import NewUser from '../Components/NewUser'
+import NewQuestion from '../Components/NewQuestion'
+import NewCategory from '../Components/NewCategory'
+import NewTopic from '../Components/NewTopic'
 
 export default function RegisterPage() {
   return (
     <Page>
-      <p>hier kommen neue Nutzer hinzu</p>
+      <MainAdmin>
+        <NewUser />
+        <NewQuestion />
+        <NewCategory />
+        <NewTopic />
+      </MainAdmin>
       <BottomNavAdmin />
     </Page>
   )
