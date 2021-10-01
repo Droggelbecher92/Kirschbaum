b597840787257c6b9075347fcfca186af97449cf
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-10-01 12:41:55 +0200

    Even more styles

---
+++ frontend/src/Components/ChooseFieldThumb.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/macro'
+
+export default styled.main`
+  display: grid;
+  grid-template-columns: 47% 47%;
+  grid-auto-rows: 18.5% 37% 18.5% 20%;
+  grid-auto-columns: initial;
+  grid-gap: var(--size-l);
+  padding: var(--size-xl);
+  height: 100%;
+  width: 100%;
+`
