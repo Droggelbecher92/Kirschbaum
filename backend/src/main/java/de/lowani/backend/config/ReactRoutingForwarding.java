f8687c00bd3edcbf332b5dc459db1eecfd77084a
Droggelbecher92 <thomaskittlaus@gmail.com>
2021-12-08 10:08:58 +0100

    Try deploy 5(try React routing)

---
+++ backend/src/main/java/de/lowani/backend/config/ReactRoutingForwarding.java
@@ -0,0 +1,13 @@
+package de.lowani.backend.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactRoutingForwarding {
+
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String forwardToRoutUrl() {
+        return "forward:/";
+    }
+}
